efc1506f35d271f37471be30d3bd6aacefe8f18e
Pavel <m83819@gmail.com>
2022-08-17 17:40:53 +0700

    Task002_DigitsSum

---
+++ Task002_DigitsSum/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+
+*/
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readValue=Console.ReadLine();
+    return int.Parse(readValue);
+}
+
+int DigitsSum(int num)
+{
+    int answer=0;
+    while(Math.Abs(num)>0)
+    {
+        answer+=Math.Abs(num)%10;
+        num/=10;
+    }
+
+    return answer;
+}
+
+int val=Prompt("enter your number: ");
+
+
+System.Console.WriteLine($" sum of your number digits is: {DigitsSum(val)}");
